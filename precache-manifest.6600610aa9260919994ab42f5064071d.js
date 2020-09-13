c01fc059cb50429feea5f4055dbc1fe95fe16723
S. Jacob Powell <s.jacob.powell@gmail.com>
2020-09-13 15:16:21 -0600

    Updates

---
+++ precache-manifest.6600610aa9260919994ab42f5064071d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2e994ebd2fc0ef8c421421e5696819f9",
+    "url": "/puzzle-charms-support/index.html"
+  },
+  {
+    "revision": "9b9e53ceac4212297ebc",
+    "url": "/puzzle-charms-support/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "082a615f41b810a80c98",
+    "url": "/puzzle-charms-support/static/js/2.0876a9fa.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/puzzle-charms-support/static/js/2.0876a9fa.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9b9e53ceac4212297ebc",
+    "url": "/puzzle-charms-support/static/js/main.c4c17625.chunk.js"
+  },
+  {
+    "revision": "c0a57908671e9302b33d",
+    "url": "/puzzle-charms-support/static/js/runtime-main.4bb3d876.js"
+  }
+]);
