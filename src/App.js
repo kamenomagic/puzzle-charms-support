9d5f737d158f4e00cf7903497dd00fc6aaca79df
S. Jacob Powell <s.jacob.powell@gmail.com>
2020-09-13 15:16:01 -0600

    Add support page

--- src/App.js
+++ src/App.js
@@ -1,24 +1,15 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h2>
+        This is the support page for the Puzzle Charms/Puzzle Cleots app.
+      </h2>
+      <h2>
+        Feel free to open a new issue with any bugs or problems, please provide screenshots etc. at <a href="https://github.com/kamenomagic/puzzle-charms-support">github.com/kamenomagic/puzzle-charms-support</a>
+      </h2>
     </div>
   );
 }
